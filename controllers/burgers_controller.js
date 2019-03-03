862ea9656c0f1d5e3905c755c51625db478c2efb
Jarret Yingling <44506154+JarretYingling@users.noreply.github.com>
2019-03-02 20:31:55 -0500

    rendering handlebars main layout and index

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -3,20 +3,58 @@ const log = console.log;
 
 const express = require("../server.js");
 const db = require("../models/burger.js");
+const path = require("path");
 
 const router = (app) => {
+    /*
+    // http default
+    app.get("/", (req, res) => {
+        log(req.body);
+        res.sendFile(path.join(__dirname, "../public/assets/test.html"));
+    });
+    */
+    // http default
+    app.get("/", (req, res) => {
+        log(req.body);
+        res.render("index");
+    });
     // http get
     app.get("/api/burgers", (req, res) => {
+        log(req.body);
         db.getBurgers(req.body, (sqlResult) => {
-            log("burgers-controller:");
+            log("burgers-controller.getBurgers:");
             log(sqlResult);
             res.json(sqlResult);
-        })
-    })
+        });
+    });
     // http post
-
+    app.post("/api/burgers", (req, res) => {
+        log(req.body);
+        db.addBurger(req.body, (sqlResult) => {
+            log("burgers-controller.addBurger:");
+            log(sqlResult);
+            res.json(sqlResult);
+        });
+    });
     // http put
-}
+    app.put("/api/burgers", (req, res) => {
+        log(req.body);
+        db.devourBurger(req.body, (sqlResult) => {
+            log("burgers-controller.devourBurger:");
+            log(sqlResult);
+            res.json(sqlResult);
+        });
+    });
+    // http delete
+    app.delete("/api/burgers/:id", (req, res) => {
+        log(req.body);
+        db.vomitBurger(req.params.id, (sqlResult) => {
+            log("burgers-controller.vomitBurger:");
+            log(sqlResult);
+            res.json(sqlResult);
+        });
+    });
+};
 
 // exports
 module.exports = router;
--- server.js
+++ server.js
@@ -8,8 +8,14 @@ const app = express();
 // require express-handlebars
 const hdb = require("express-handlebars");
 
+// require path
+const path = require("path");
+
 // dynamic port or default 8080
 const PORT = process.env.PORT || 8080;
+
+// set static root to /public
+app.use(express.static(path.join(__dirname, "/public")));
 
 // express data parsing
 app.use(express.urlencoded({
@@ -17,12 +23,14 @@ app.use(express.urlencoded({
 }));
 app.use(express.json());
 
+// configure express-handlebars
+app.engine("handlebars", hdb({
+    defaultLayout: "main"
+}));
+app.set("view engine", "handlebars");
+
 // routes
 require("./controllers/burgers_controller.js")(app);
-
-// configure express-handlebars
-app.engine("handlebars", hdb());
-app.set("view engine", "handlebars");
 
 // invoke express app listener
 app.listen(PORT, () => {
